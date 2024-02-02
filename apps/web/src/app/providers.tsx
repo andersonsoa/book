aae7a25d14c6baf76c7982061eb0fdbbce0df5da
Anderson Soares <anderson.soares@defensoria.ro.def.br>
2024-02-02 12:43:28 -0400

    remove material ui

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,25 +1,15 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from "@mui/material";
-import { themeDark, themeLight } from "@/lib/theme";
-import { useState } from "react";
 
 type Props = {
   children?: React.ReactNode;
 };
 
 export function Providers({ children }: Props) {
-  const [themaSelecionado, setThemaSelecionado] = useState(themeDark);
-
-  const toggleDark = () => setThemaSelecionado(themeDark);
-  const toggleLight = () => setThemaSelecionado(themeLight);
-
   return (
     <>
-      <ThemeProvider theme={themaSelecionado}>
-        <SessionProvider>{children}</SessionProvider>
-      </ThemeProvider>
+      <SessionProvider>{children}</SessionProvider>
     </>
   );
 }
